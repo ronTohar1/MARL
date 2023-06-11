97cfe0c50804c1c2682c1836785a28a9f92cf0b2
Ron Tohar <rontoh@post.bgu.ac.il>
2023-06-11 16:38:07 +0300

    Maze new implementation

--- maze.py
+++ maze.py
@@ -1,7 +1,8 @@
 import gym
 import random
 import networkx as nx
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Maze(gym.Env):
@@ -9,10 +10,11 @@ class Maze(gym.Env):
         
         Maze._check_legality(dim, num_of_agents, density)
 
-        self.W = "W"
-        self.E = "-"
+        self.W = "■ "
+        self.E = "□ "
         self.Agent = "A"
         self.Goal = "G"
+        self.Blocking_Wall = "▢"
 
         self.UP = 0
         self.DOWN = 1
@@ -23,19 +25,27 @@ class Maze(gym.Env):
         self.num_of_agents = num_of_agents
         self.density = density
 
-        self.maze = []
+        self.maze = None
         self.agents = []
         self.goals = []
         self.graph = None
+
+        self.observation_space = 
 
 
     def _check_legality(dim, num_agents, density):
         if dim < 2 or dim > 20:
             raise Exception("Dimension must be between 2 and 20")
-        num_walls = int(density * dim * dim)
-        if num_walls + num_agents*2 > dim * dim:
-            raise Exception("Number of walls + Agents + Goals is greater than the dimension of the maze")
+        if num_agents*2 > dim * dim/2:
+            raise Exception("Number of agents + goals is greater than half the dimension of the maze")
+        # num_walls = int(density * dim * dim)
+        # if num_walls + num_agents*2 > dim * dim:
+            # raise Exception("Number of walls + Agents + Goals is greater than the dimension of the maze")
         
+    def _draw_graph(self):
+        nx.draw(self.graph, with_labels=True)
+        plt.show()
+    
     def _get_empty_places(self, location_as_tuple=False):
         if location_as_tuple:
             return [(i,j) for i in range(self.dim) for j in range(self.dim) if self.maze[i][j] == self.E]
@@ -54,30 +64,6 @@ class Maze(gym.Env):
         entities = random.sample(empty_places, num_of_entities)
         entities = [self._convert_location_to_tuple(entity) for entity in entities]
         return entities         
-
-    def check_paths(self, maze, agents, goals):
-        G = nx.Graph()
-        for i in range(len(maze)):
-            for j in range(len(maze)):
-                if maze[i][j] != self.W:
-                    G.add_node((i,j))
-        for i in range(len(maze)):
-            for j in range(len(maze)):
-                if maze[i][j] != self.W:
-                    if i > 0 and maze[i-1][j] != self.W:
-                        G.add_edge((i,j), (i-1,j))
-                    if i < len(maze)-1 and maze[i+1][j] != self.W:
-                        G.add_edge((i,j), (i+1,j))
-                    if j > 0 and maze[i][j-1] != self.W:
-                        G.add_edge((i,j), (i,j-1))
-                    if j < len(maze)-1 and maze[i][j+1] != self.W:
-                        G.add_edge((i,j), (i,j+1))
-        for i in range(len(agents)):
-            if nx.has_path(G, agents[i], goals[i]) == False:
-                return False
-        return True
-
-
     
     def _get_agent_name(self, agent):
         return self.Agent + str(self.agents.index(agent))
@@ -86,59 +72,102 @@ class Maze(gym.Env):
         return self.Goal + str(self.goals.index(goal))
     
     def _reset_maze(self):
-        self.maze = [[self.E for _ in range(self.dim)] for _ in range(self.dim)]
+        self.maze = np.full((self.dim, self.dim), self.E, dtype=object)
     
-    def _add_agents(self, agents):
-        for agent in agents:
-            self.maze[agent[0]][agent[1]] = self._get_agent_name(agent) 
+    def _add_agents(self):
+        if self.agents is None:
+            raise Exception("Agents are not initialized")
+        for agent in self.agents:
+            name = self._get_agent_name(agent) 
+            self.maze[agent] = name
 
-    def _add_goals(self, goals):
-        for goal in goals:
-            self.maze[goal[0]][goal[1]] = self._get_goal_name(goal)
+    def _add_goals(self):
+        if self.goals is None:
+            raise Exception("Goals are not initialized")
+        for goal in self.goals:
+            self.maze[goal] = self._get_goal_name(goal)
 
-    def _create_graph(self,):
+    """ Creates the initial graph of the maze, after removing edges that go into or out of a wall or agent"""
+    def _create_initial_graph(self,):
+
+        def is_wall(cell):
+            return self.maze[cell] == self.W
+        
+        def is_agent(cell):
+            return self.maze[cell].startswith(self.Agent)
+        
+        def is_wall_or_agent(cell):
+            return is_wall(cell) or is_agent(cell)
+
         self.graph = nx.grid_graph(dim=(self.dim, self.dim))
 
+        # Remove all edges that go into or out of a wall or agent
+        edges_to_remove = [e for e in self.graph.edges if is_wall_or_agent(e[0]) or is_wall_or_agent(e[1])]
+        self.graph.remove_edges_from(edges_to_remove)
+
+    def _get_adjacent_cells(self, cell):
+        adjacent_cells = [(cell[0] - 1, cell[1]), (cell[0] + 1, cell[1]), (cell[0], cell[1] - 1), (cell[0], cell[1] + 1)]
+        adjacent_cells = [cell for cell in adjacent_cells if cell[0] >= 0 and cell[0] < self.dim and cell[1] >= 0 and cell[1] < self.dim]
+        return adjacent_cells
+
+    """ Checks if there is a path from each agent to its goal """
+    def _agents_has_path_to_goal(self):
+        for agent in self.agents:
+            # add agent edges to the graph
+            agent_edges = [(agent, neighbor) for neighbor in self._get_adjacent_cells(agent)]
+            self.graph.add_edges_from(agent_edges)
+            goal = self.goals[self.agents.index(agent)]
+
+            if not nx.has_path(self.graph, agent, goal):
+                return False
+            
+            # remove agent edges from the graph
+            self.graph.remove_edges_from(agent_edges)
+
+        return True
+
+    """ Tries to add wall and checks if there is still a path from each agent to its goal"""
+    def _add_wall(self, wall):
+        if self.maze[wall] != self.E:
+            return False
+        
+        # remove wall edges from the graph
+        wall_edges = [(wall, neighbor) for neighbor in self.graph.neighbors(wall)]
+        self.graph.remove_edges_from(wall_edges)
+        if not self._agents_has_path_to_goal():
+            # add wall edges to the graph
+            self.graph.add_edges_from(wall_edges)
+            self.maze[wall] = self.Blocking_Wall # mark the wall as a place that cannot be a wall
+            return False
+        
+        # add wall to the maze
+        self.maze[wall] = self.W
+
+        return True
 
     def _generate_maze(self, tries=50):
-        if self.density < 0 or self.density > 0.5:
-                    raise Exception("Percent of walls must be between 0 and 0.5")
-        
         # reset the maze
         self._reset_maze()
         self.agents = self._generate_entities(self.num_of_agents)
-        self._add_agents(self.agents)
+        self._add_agents()
         self.goals = self._generate_entities(self.num_of_agents)
-        self._add_goals(self.goals)
-        self._create_graph()
+        self._add_goals()
+        self._create_initial_graph()
         
         number_of_walls = int(self.density * self.dim * self.dim)
+        print("Number of walls to place: {}".format(number_of_walls))
 
         # place the walls in the maze
-        no_more_empty_places = False
-        for _ in range(number_of_walls):
-            if no_more_empty_places:
-                break
-            for _ in range(tries):
+        placed_walls = 0
+        while placed_walls < number_of_walls:
                 walls = self._generate_entities(1)
-                if walls is None:
-                    no_more_empty_places = True
+                if walls is None: # No more places to place a wall
+                    print("Cannot place more walls, maze is full\nPlaced {} walls out of {}".format(placed_walls, number_of_walls))
                     break
-                if maze[x][y] == 0:
-                    maze[x][y] = self.W
-                    if self.check_paths(maze, self.agents, self.goals) == False:
-                        maze[x][y] = 0
-                    else:
-                        number_of_walls -= 1
-                        break
-        
-        # place the empty spaces in the maze
-        for i in range(self.dim):
-            for j in range(self.dim):
-                if maze[i][j] == 0:
-                    maze[i][j] = self.E
-        return maze
-
+                wall = walls[0]
+                if self._add_wall(wall):
+                    placed_walls += 1
+       
     def get_maze(self):
         return self.maze
     
@@ -148,7 +177,7 @@ class Maze(gym.Env):
         # self.goals = self.generate_goals(num_of_agents)
 
         # self.maze = self.generate_maze(percent_of_walls)
-        self.generate_maze()
+        self._generate_maze()
 
     def step(self, action):
         # for each agent update the maze to move according to the action
@@ -185,8 +214,8 @@ class Maze(gym.Env):
 if __name__ == '__main__':
     import time
     start_time = time.time()
-    m = Maze()
-    m.reset(10, 6, 0.6)
+    m = Maze(10, 6, density=0.3)
+    m.reset()
     total_time = time.time() - start_time
     print("Time taken to generate maze: ", total_time)
     m.render()
